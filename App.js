99677ef46d791e67f1ee4d08ad3aca6a08777f6d
KaranMalviya <karanmalviya258@gmail.com>
2022-07-21 23:14:31 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import Hello from './components/Hello';
+
+function App() {
+  return (
+    <View>
+      {/* <Text>Hello React native</Text> */}
+      <Hello />
+    </View>
+  );
+}
+
+export default App;
